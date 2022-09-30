36f3bdc2698f95f23d2e4be2cc64b7d0928707c0
Anon772000 <hdbreen@gmail.com>
2022-09-30 15:56:51 +1000

    updated template

--- html/assets/inc/CheckOther.inc.php
+++ html/assets/inc/CheckOther.inc.php
@@ -9,9 +9,8 @@ if($_SERVER['HTTP_HOST'] == "dg-bells.djarragun.college"){
         $status = "Wangetti Status: <span class='w3-tag w3-red'>Offline</span>";
     }
 }elseif ($_SERVER['HTTP_HOST'] == "dw-bells.djarragun.college") {
-    $url = "http://dw-bells.djarragun.college/";
-    $headers = @get_headers($url);
-    $locateOther = "Gordonvale" 
+    $url = "http://dg-bells.djarragun.college/";
+    $headers = @get_headers($url); 
     if($headers && strpos( $headers[0], '200')) {
         $status = "Gordonvale Status: <span class='w3-tag w3-green'>Online</span>";
     }
