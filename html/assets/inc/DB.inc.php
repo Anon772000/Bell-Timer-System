fe09814cf5cfd80e05735b9e55a06cc0809ff947
Anon772000 <hdbreen@gmail.com>
2022-11-03 00:27:55 +1100

    Updated server URL

--- html/assets/inc/DB.inc.php
+++ html/assets/inc/DB.inc.php
@@ -1,7 +1,7 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'dg-bells.djarragun.college');
+define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'YxNr9FHeCrp8gUrAsVmd');
 define('DB_NAME', 'accounts');
@@ -13,4 +13,4 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-?>
+?>YxNr9FHeCrp8gUrAsVmd
